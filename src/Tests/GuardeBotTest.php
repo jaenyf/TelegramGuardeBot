9ce673010dd806783296d30a0a4a52aaced956b9
jaenyf <95911656+jaenyf@users.noreply.github.com>
2022-01-08 20:08:48 +0100

    Add tests for GuardeBot

--- src/Tests/GuardeBotTest.php
+++ src/Tests/GuardeBotTest.php
@@ -32,4 +32,73 @@ final class GuardeBotTest extends TestCase
         //Act / Assert
         new GuardeBot($telegramApiStub, '', GuardeBotTest::logChatTestId);
     }
+
+    public function testIsHookedUseApi(): void
+    {
+        //Arrange
+        $telegramApiStub = $this->createMock(TelegramApi::class);
+
+        $telegramApiStub->expects($this->once())->method('getWebhookInfo');
+
+        //Act / Assert
+        $sut = new GuardeBot($telegramApiStub, GuardeBotTest::hookTestName, GuardeBotTest::logChatTestId);
+        $sut->isHooked();
+    }
+
+    public function testHookUseApi(): void
+    {
+        //Arrange
+        $url = 'http://web.ho.ok';
+        $certificate = 'cert';
+        $dropPendingUpdates = true;
+        $telegramApiStub = $this->createStub(TelegramApi::class);
+
+        $webHookInfo = new \stdClass();
+        $webHookInfo->url = '';
+        $telegramApiStub->method('getWebhookInfo')
+             ->willReturn($webHookInfo);
+
+        $telegramApiStub->expects($this->once())
+            ->method('setWebhook')
+            ->with($this->equalTo($url, $certificate, null, null, null, $dropPendingUpdates));
+
+        //Act / Assert
+        $sut = new GuardeBot($telegramApiStub, GuardeBotTest::hookTestName, GuardeBotTest::logChatTestId);
+        $sut->hook($url, $certificate, $dropPendingUpdates);
+    }
+
+    public function testSayUseApi(): void
+    {
+        //Arrange
+        $telegramApiStub = $this->createMock(TelegramApi::class);
+
+        $telegramApiStub->expects($this->once())
+            ->method('sendMessage')
+            ->with($this->equalTo(['chat_id' => GuardeBotTest::logChatTestId, 'text' => 'hello world !']));
+
+        //Act / Assert
+        $sut = new GuardeBot($telegramApiStub, GuardeBotTest::hookTestName, GuardeBotTest::logChatTestId);
+        $sut->say(GuardeBotTest::logChatTestId, 'hello world !');
+    }
+
+    public function testUnhookUseApi(): void
+    {
+        //Arrange
+        $dropPendingUpdates = true;
+        $telegramApiStub = $this->createStub(TelegramApi::class);
+
+        // Configure getWebhookInfo
+        $webHookInfo = new \stdClass();
+        $webHookInfo->url = 'http://web.ho.ok';
+        $telegramApiStub->method('getWebhookInfo')
+             ->willReturn($webHookInfo);
+
+        $telegramApiStub->expects($this->once())
+            ->method('deleteWebhook')
+            ->with($this->equalTo($dropPendingUpdates));
+
+        //Act / Assert
+        $sut = new GuardeBot($telegramApiStub, GuardeBotTest::hookTestName, GuardeBotTest::logChatTestId);
+        $sut->unHook($dropPendingUpdates);
+    }
 }
